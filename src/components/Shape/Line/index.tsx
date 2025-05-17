8602f0924969db7a9177008d7c51af078fdb9d21
halionaz <samhsj@korea.ac.kr>
2025-05-17 16:14:21 +0900

    fix: 없는 모듈 참조

--- src/components/Shape/Line/index.tsx
+++ src/components/Shape/Line/index.tsx
@@ -1,14 +1,11 @@
 import { forwardRef } from 'react';
 import { ShapeViewProps } from '@/libs/types';
 
-import s from './Line.module.css';
-
 const Line = forwardRef<HTMLDivElement, ShapeViewProps>(
   ({ style, onMouseDown }, ref) => {
     return (
       <div
         ref={ref}
-        className={s.Line}
         style={{ ...style, height: '2px' }}
         onMouseDown={onMouseDown}
       />
